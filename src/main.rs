94ebe161cbc3860bf64b355d23fa9d87739ff673
mxngls <Maximilian.e.hoenig@gmail.com>
2025-07-25 16:23:27 +0900

    Only operate on target stash

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ struct GitRenameStash {
 
     /// the stash whose message we intent to edit
     #[argh(positional, greedy)]
-    stash: u16,
+    stash: usize,
 }
 
 fn main() -> Result<(), git2::Error> {
@@ -25,48 +25,49 @@ fn main() -> Result<(), git2::Error> {
 
     let mut repo = Repository::open(repository)?;
 
-    // collect stashes
-    let mut stashes = Vec::new();
+    // get target stash
+    let mut target_stash: Option<(usize, String, Oid)> = None;
     let stash_cb = |index: usize, message: &str, id: &Oid| -> bool {
-        stashes.push((index, message.to_string(), *id));
-        index != stash_index.into()
+        if index == stash_index {
+            target_stash = Some((index, message.to_string(), *id));
+            false
+        } else {
+            true
+        }
     };
     repo.stash_foreach(stash_cb)?;
 
-    for (index, _message, id) in stashes {
-        if index != stash_index.into() {
-            continue;
-        }
+    let (index, _old_message, commit_id) =
+        target_stash.ok_or_else(|| git2::Error::from_str("Stash not found"))?;
 
-        let commit = repo.find_commit(id)?;
-        let tree = commit.tree()?;
-        let parents = commit
-            .parents()
-            .map(|p| repo.find_commit(p.id()))
-            .collect::<Result<Vec<_>, _>>()?;
-        let parent_refs: Vec<&git2::Commit> = parents.iter().collect();
+    let commit = repo.find_commit(commit_id)?;
+    let tree = commit.tree()?;
+    let parents = commit
+        .parents()
+        .map(|p| repo.find_commit(p.id()))
+        .collect::<Result<Vec<_>, _>>()?;
+    let parent_refs: Vec<&git2::Commit> = parents.iter().collect();
 
-        let new_commit = repo.commit(
-            None,
-            &commit.author(),
-            &commit.committer(),
-            &new_message,
-            &tree,
-            &parent_refs,
-        )?;
+    let new_commit = repo.commit(
+        None,
+        &commit.author(),
+        &commit.committer(),
+        &new_message,
+        &tree,
+        &parent_refs,
+    )?;
 
-        // let us work with `repo` in peace
-        drop(commit);
-        drop(tree);
-        drop(parents);
+    // let us work with `repo` in peace
+    drop(commit);
+    drop(tree);
+    drop(parents);
 
-        // drop existing stash
-        repo.stash_drop(index)?;
+    // drop existing stash
+    repo.stash_drop(index)?;
 
-        // update stash references
-        // NOTE: the log messages of the stash reflog default to the commit messages of each stash
-        repo.reference("refs/stash", new_commit, true, &new_message)?;
-    }
+    // update stash references
+    // NOTE: the log messages of the stash reflog default to the commit messages of each stash
+    repo.reference("refs/stash", new_commit, true, &new_message)?;
 
     Ok(())
 }
